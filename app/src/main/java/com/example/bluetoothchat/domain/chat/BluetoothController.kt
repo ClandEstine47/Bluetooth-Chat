455480c194c09f5e6f979409398376198990eb50
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2023-10-04 14:52:57 +0545

    domain layer added

---
+++ app/src/main/java/com/example/bluetoothchat/domain/chat/BluetoothController.kt
@@ -0,0 +1,14 @@
+package com.example.bluetoothchat.domain.chat
+
+import kotlinx.coroutines.flow.StateFlow
+
+interface BluetoothController {
+
+    val scannedDevices: StateFlow<List<BluetoothDeviceDomain>>
+    val pairedDevices: StateFlow<List<BluetoothDeviceDomain>>
+
+    fun startDiscovery()
+    fun stopDiscovery()
+    fun release()
+
+}
