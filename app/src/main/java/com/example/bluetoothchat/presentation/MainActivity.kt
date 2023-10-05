5b4f781fb1f87a5b5b3e81ea30f22a2715f7284e
ClandEstine47 <mausamsarumagar20@gmail.com>
2023-10-05 17:24:37 +0545

    bluetooth permission setup

--- app/src/main/java/com/example/bluetoothchat/presentation/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothchat/presentation/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.bluetoothchat.presentation
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -14,12 +21,53 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val bluetoothManager by lazy {
+        applicationContext.getSystemService(BluetoothManager::class.java)
+    }
+    private val bluetoothAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BluetoothChatTheme {
                 val viewModel = hiltViewModel<BluetoothViewModel>()
                 val state by viewModel.state.collectAsState()
+
+                val enabledBluetoothLauncher = registerForActivityResult(
+                    ActivityResultContracts.StartActivityForResult()
+                ){}
+
+                val permissionLauncher = registerForActivityResult(
+                    ActivityResultContracts.RequestMultiplePermissions()
+                ){ perms ->
+                    val canEnableBluetooth = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                        perms[Manifest.permission.BLUETOOTH_CONNECT] == true
+                    } else true
+
+                    if (canEnableBluetooth && !isBluetoothEnabled) {
+                        enabledBluetoothLauncher.launch(
+                            Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                        )
+                    }
+                }
+
+                LaunchedEffect(true) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                        permissionLauncher.launch(
+                            arrayOf(
+                                Manifest.permission.BLUETOOTH_SCAN,
+                                Manifest.permission.BLUETOOTH_CONNECT
+                            )
+                        )
+                    }
+                }
+
 
                 Surface(
                     color = MaterialTheme.colorScheme.background
