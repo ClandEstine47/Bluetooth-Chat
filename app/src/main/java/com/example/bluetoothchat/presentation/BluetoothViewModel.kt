22bef09eb842418438528c45ffb4a9ac04f52c37
ClandEstine47 <mausamsarumagar20@gmail.com>
2023-10-07 14:02:41 +0545

    fun connectToDevice | disconnectFromDevice created in viewmodel

--- app/src/main/java/com/example/bluetoothchat/presentation/BluetoothViewModel.kt
+++ app/src/main/java/com/example/bluetoothchat/presentation/BluetoothViewModel.kt
@@ -3,6 +3,7 @@ package com.example.bluetoothchat.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.bluetoothchat.domain.chat.BluetoothController
+import com.example.bluetoothchat.domain.chat.BluetoothDeviceDomain
 import com.example.bluetoothchat.domain.chat.ConnectionResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
@@ -34,6 +35,8 @@ class BluetoothViewModel @Inject constructor(
         )
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), _state.value)
 
+    private var deviceConnectionJob: Job? = null
+
     init {
         bluetoothController.isConnected.onEach { isConnected ->
             _state.update { it.copy(isConnected = isConnected) }
@@ -44,7 +47,21 @@ class BluetoothViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
+    fun connectToDevice(device: BluetoothDeviceDomain) {
+        _state.update { it.copy(isConnecting = true) }
+        deviceConnectionJob = bluetoothController
+            .connectToDevice(device)
+            .listen()
+    }
 
+    fun disconnectFromDevice() {
+        deviceConnectionJob?.cancel()
+        bluetoothController.closeConnection()
+        _state.update { it.copy(
+            isConnected = false,
+            isConnecting = false
+        ) }
+    }
 
     fun startScan() {
         bluetoothController.startDiscovery()
